ed85ba8357ca0782c55e1b47294d12919f0ba861
yameste <yameste@yahoo.co.uk>
2018-06-20 18:11:15 +0100

    added input field that commits to vuex store

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="hello">
+    <div class="left">
+        <h1>{{ title }}</h1>
+
+        <form @submit.prevent="addLink">
+          <input class="link-input" type="text" placeholder="Add a Link" v-model="newLink">
+        </form>
+
+        <ul>
+            <li v-for="(link, index) in links" v-bind:key="index">
+              {{ link }}
+            </li>
+
+        </ul>
+    </div>
+
+    <div class="right">
+        <stats></stats>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {mapState, mapMutations} from 'vuex';
+  import Stats from '@/components/Stats.vue'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      newLink: ''
+    }
+
+  },
+  components: {
+    Stats
+  },
+  computed: {
+    ...mapState([
+      'title',
+      'links'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'ADD_LINK'
+    ]),
+    addLink: function() {
+      this.ADD_LINK(this.newLink)
+      this.newLink = ''
+
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html, #app, .home {
+    height: 100%;
+  }
+  body {
+    background-color: #F4F4F4;
+    margin: 0;
+    height: 100%;
+  }
+
+  .hello {
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+    grid-template-rows: 100%;
+    grid-template-areas:
+      "left right";
+    height: 100%;
+  }
+
+  .left, .right {
+    padding: 30px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  ul li {
+    padding: 20px;
+    background: white;
+    margin-bottom: 8px;
+  }
+
+  .right {
+    grid-area: right;
+    background-color: #E9E9E9;
+  }
+
+  input {
+    border: none;
+    padding: 20px;
+    width: calc(100% - 40px);
+    box-shadow: 0 5px 5px lightgrey;
+    margin-bottom: 50px;
+    outline: none;
+  }
+</style>
---
+++ src/store.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title: 'My Custom Title',
+    links: [
+      'http://google.com',
+      'http://coursetro.co',
+      'http://youtube.com'
+    ]
+
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+
+    }
+
+  },
+
+  mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    }
+
+  },
+  actions: {
+
+  }
+})
